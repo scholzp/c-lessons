04c866f48f60d1ae1bbf023e097ceae1b7c22ee6
MirkoJa <mirko.jantschke@gmx.de>
2018-12-19 15:24:52 +0100

    ADD: commented code

--- solutions/Santas_sledge/main.c
+++ solutions/Santas_sledge/main.c
@@ -16,7 +16,7 @@ struct present {
 
 struct sledge {
   struct reindeer deers[8];
-  struct present presents[10];
+  struct present presents[20];
   int maxTransportWeight;
 };
 
@@ -31,41 +31,49 @@ int main(void) {
   // init sledge
   struct sledge santasSledge;
 
-  // init weight
+  // init weight of all deers with 0
   for (size_t i = 0; i < (sizeof(santasSledge.deers)/sizeof(struct reindeer)); i++) {
     santasSledge.deers[i].pullForce = 0;
   }
-
+  // init weight of all presents with 0
   for (size_t i = 0; i < (sizeof(santasSledge.presents)/sizeof(struct present)); i++) {
     santasSledge.presents[i].weight = 0;
     santasSledge.presents[i].price = 0;
   }
 
+
+  // set santasSledge variables
   santasSledge.maxTransportWeight = 500;
   santasSledge.presents[0] = computer;
   santasSledge.presents[1] = bookshelf;
   santasSledge.presents[2] = fridge;
   santasSledge.deers[0] = rudolph;
 
+  // resultvariables for calculation
   int pullForceSum = 0;
   int presentWeightSum = 0;
   float presentPriceSum = 0;
 
+  // iterate through all reindeers and get the pullforce of each reindeer
   for (size_t i = 0; i < (sizeof(santasSledge.deers)/sizeof(struct reindeer)); i++) {
     pullForceSum += santasSledge.deers[i].pullForce;
   }
 
+  //iterate through all presents and get the weight and the price of all presents
   for (size_t i = 0; i < (sizeof(santasSledge.presents)/sizeof(struct present)); i++) {
     presentWeightSum += santasSledge.presents[i].weight;
     presentPriceSum += santasSledge.presents[i].price;
   }
 
+  // check if the deers have enough force to pull the sledge
   if (presentWeightSum <= pullForceSum) printf("The reindeers have enough force to pull the sledge\n");
   else printf("The reindeers dont have enough force to pull the sledge\nYou should add more reindeers!\n\n");
 
   printf("pullForce: %i Kg\n", pullForceSum);
   printf("presentWeightSum: %i Kg\n", presentWeightSum);
+  printf("force that is still needed: %i Kg\n", presentWeightSum-pullForceSum);
 
+  //print the total price and calculate the insurance cost
   printf("\nTotal carried money worth: %f\n", presentPriceSum);
   printf("The price for the insurance of the cargo are (2%% of the money worth): %f\n", presentPriceSum*0.03);
 
