b1a48adbf234c03828bdaf99da7b2e28e136a604
scholzp <pasix@outlook.de>
2019-01-28 10:36:45 +0100

    ADD: Added debugging slides and simple excample for gdb-debugging

---
+++ materials/debugging/gdb_excample.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void a(int n){
+  if(n = 0)
+    return;
+  a(n);
+}
+
+void b(int n){
+  a(n);
+}
+
+void c(int n){
+  b(n);
+}
+
+void d(int n){
+  c(n);
+}
+
+
+int main(void){
+  int n = 10;
+  d(n);
+  return 0;
+}
